1a14234d03c3e19460dd07b9a5c90b8874be9cab
Wei Sun <weisun2013@gmail.com>
2020-05-19 13:52:59 -0700

    update prepare_COAD.R

--- TCGA_pipeline/prepare_COAD.R
+++ TCGA_pipeline/prepare_COAD.R
@@ -1,23 +1,27 @@
 
-source("~/test/source/_lib.R")
+# the raw data of DNA metylation is too large to be kept in gitHub
+# here is the local path for DNA methylation data
+path.data = "~/research/TCGA/COAD/_data2"
 
-setwd("~/Hutch-Research/Data/Processed")
+library(data.table)
+library(stringr)
 
 # ------------------------------------------------------------
 # read in pure cell type data
 # ------------------------------------------------------------
 
-info = read.table("methylation_pure_ct_info.txt", as.is=TRUE, sep="\t",
-                  header=TRUE, quote="")
+path.ref = "../cell_type_specific_reference/data"
+
+info = fread(file.path(path.ref, "methylation_pure_ct_info.txt.gz"))
 dim(info)
 info[1:2,]
 
-dat = fread("methylation_pure_ct_rmPC2_data.txt")
+dat = fread(file.path(path.ref, 
+                      "methylation_pure_ct_rmPC2_data_signif4.txt.gz"))
 dim(dat)
 dat[1:2,1:5]
 
-sam = read.table("methylation_pure_ct_sample.txt", as.is=TRUE,
-                 sep="\t", header=TRUE)
+sam = fread(file.path(path.ref, "methylation_pure_ct_sample.txt"))
 dim(sam)
 sam[1:2,]
 
@@ -31,15 +35,13 @@ rownames(dat) = info$ID
 # read DNA methylation data
 # ------------------------------------------------------------
 
-#setwd("~/research/TCGA/COAD/")
-
-datM = fread(file = "methylation_betaValue.txt", header=TRUE)
+datM = fread(file.path(path.data, "methylation_betaValue.txt"))
 dim(datM)
 datM[1:2, 1:5]
 
-infoM = read.table(file = "methylation_info.txt", sep = "\t",
-                   header = TRUE, as.is = TRUE, quote="")
+infoM = fread(file.path(path.ref, "methylation_info.txt"))
 dim(infoM)
+names(infoM)
 infoM[1:2, 1:5]
 table(infoM$CHR)
 
@@ -82,31 +84,17 @@ info[1:2, 1:5]
 # read DNA methylation sample information
 # ------------------------------------------------------------
 
-setwd("./COAD")
-
-coad = read.table("patient_coad_short_table_nMut.txt", sep="\t",
-                  as.is=TRUE, header=TRUE)
-dim(coad)
-coad[1:2,]
-
-ff0    = "patient_coad_M_info_hyperMeth.txt"
+ff0    = "../TCGA_results/clinical_data/patient_coad_M_info_hyperMeth.txt"
 emInfo = read.table(ff0, sep = "\t", header = TRUE, as.is = TRUE)
 dim(emInfo)
 emInfo[1, ]
-
-table(emInfo$bcr_patient_barcode %in% coad$bcr_patient_barcode)
-mat1 = match(emInfo$bcr_patient_barcode, coad$bcr_patient_barcode)
-emInfo$nMut = coad$nMut[mat1]
-
-table(colnames(datM) == emInfo$patient_id)
 
 # ------------------------------------------------------------
 # read in probes to be used
 # ------------------------------------------------------------
 
-setwd("../../../R_batch2")
-
-load("ref_966probes.RData")
+load(file.path(path.ref, "ref_966probes.RData"))
+ls()
 genes = probe2use
 length(genes)
 length(unique(genes))
@@ -123,6 +111,7 @@ dim(sam)
 table(sam$id == colnames(X))
 table(sam$label)
 cellTypes = unique(sam$label)
+
 # ------------------------------------------------------------
 # extract methylation data from tumor samples
 # ------------------------------------------------------------
@@ -131,24 +120,31 @@ ys = datM[match(rownames(X), rownames(datM)),]
 dim(ys)
 ys[1:2,1:5]
 
-ys_na = which(apply(is.na(ys),2,any))
+ys_na    = which(apply(is.na(ys),2,any))
 eta_abs  = emInfo$abs_purity
 eta_abs_na = which(is.na(eta_abs))
-nona = c(ys_na,eta_abs_na)
-ys = ys[,-nona]
-eta_abs = eta_abs[-nona]
+
+any.na = union(ys_na,eta_abs_na)
+any.na
+
+ys = ys[,-any.na]
+eta_abs = eta_abs[-any.na]
+
+dim(ys)
 
 #-------------------------------------------------------------
 # Estimate Mean Matrix mu
 #-------------------------------------------------------------
-mu = matrix(NA, nrow = dim(X)[1],ncol = length(cellTypes))
-s2 = matrix(NA, nrow = dim(X)[1],ncol = length(cellTypes))
 
-row.names(mu) = row.names(s2) =rownames(X)
-colnames(mu) = colnames(s2) = cellTypes
+mu = matrix(NA, nrow = dim(X)[1], ncol = length(cellTypes))
+s2 = matrix(NA, nrow = dim(X)[1], ncol = length(cellTypes))
+
+row.names(mu) = row.names(s2) = rownames(X)
+colnames(mu)  = colnames(s2)  = cellTypes
 
 for(ct in cellTypes){
-  dat.ct = X[,sam[which(sam[,2]==ct),1]]
+  sam.ct = unlist(sam[which(sam[,2]==ct),1])
+  dat.ct = X[,sam.ct]
   mu[,ct] = rowMeans(dat.ct,na.rm=TRUE)
   s2[,ct] = apply(dat.ct,1,sd,na.rm = TRUE)^2
 }
@@ -156,94 +152,42 @@ for(ct in cellTypes){
 #-------------------------------------------------------------
 # Read Estimation from Expression Data
 #-------------------------------------------------------------
-setwd('~/Hutch-Research/Data/Real')
-est_expr <- fread('COAD_composition_cibersortx.txt')
-samname <- as.list(est_expr[,1])
-samname <- samname$Mixture
-for(i in 1:length(samname)){
-  samname[i] <- str_replace(samname[i],"X","")
-}
+
+fnm = '../TCGA_results/deconv_results/COAD_composition_cibersortx.txt'
+est_expr = fread(fnm)
+dim(est_expr)
+est_expr[1:2,]
+
+samname  = str_replace(est_expr$Mixture, "^X", "")
 length(samname)
+samname[1:5]
 
-com_sample <- intersect(samname,colnames(ys))
+com_sample = intersect(samname,colnames(ys))
+length(com_sample)
 
-est_expr <- est_expr[which(samname %in% com_sample),-1]
-rownames(est_expr) <- samname[which(samname %in% com_sample)]
+w2kp = which(samname %in% com_sample)
+est_expr = data.matrix(est_expr[w2kp,-1])
+rownames(est_expr) = samname[w2kp]
+dim(est_expr)
+est_expr[1:2,]
 
-deconv_expr <- matrix(NA, nrow = nrow(est_expr),ncol = length(cellTypes))
-colnames(deconv_expr) <- cellTypes 
-rownames(deconv_expr) <- rownames(est_expr)
-other <- rowSums(est_expr[,c(10,19,20,21)])
-deconv_expr[,"B"] <- rowSums(est_expr[,1:3])/0.4
-deconv_expr[,"CD4T"] <- rowSums(est_expr[,5:8])/0.4
-deconv_expr[,"CD8T"] <- as.matrix(est_expr[,4])/0.4
-deconv_expr[,"Treg"] <- as.matrix(est_expr[,9])/0.4
-deconv_expr[,"NK"] <- rowSums(est_expr[,11:12])/0.42
-deconv_expr[,"Monocyte"] <- rowSums(est_expr[,13:18])/1.40
-deconv_expr[,"Neutrophil"] <- as.matrix(est_expr[,"Neutrophils"])/0.15
-#est_expr$`T cells gamma delta` <- est_expr$`T cells gamma delta`
-#est_expr$Eosinophils <- est_expr$Eosinophils
-#est_expr$`Mast cells resting` <- est_expr$`Mast cells resting`
-#est_expr$`Mast cells activated` <- est_expr$`Mast cells activated`
-deconv_expr <- deconv_expr / rowSums(deconv_expr)
+deconv_expr = matrix(NA, nrow = nrow(est_expr),ncol = length(cellTypes))
+colnames(deconv_expr) = cellTypes 
+rownames(deconv_expr) = rownames(est_expr)
+colnames(est_expr)
+
+other = rowSums(est_expr[,c(10,19,20,21)])
+deconv_expr[,"B"] = rowSums(est_expr[,1:3])/0.4
+deconv_expr[,"CD4T"] = rowSums(est_expr[,5:8])/0.4
+deconv_expr[,"CD8T"] = as.matrix(est_expr[,4])/0.4
+deconv_expr[,"Treg"] = as.matrix(est_expr[,9])/0.4
+deconv_expr[,"NK"] = rowSums(est_expr[,11:12])/0.42
+deconv_expr[,"Monocyte"] = rowSums(est_expr[,13:18])/1.40
+deconv_expr[,"Neutrophil"] = as.matrix(est_expr[,22])/0.15
+deconv_expr = deconv_expr / rowSums(deconv_expr)
 
 print(dim(deconv_expr))
-
-#-----------------------------------------------------------
-# Get Barcode for each sample and check purity data
-#-----------------------------------------------------------
-#tcga_purity <- fread('tcga_purity.txt')
-#barcode = list()
-#base_string = '%s-%s-%s'
-#for(i in 1:length(tcga_purity$array)){
-#  v1 <- strsplit(tcga_purity$array[i],'-')[[1]][1:3]
-#  temp <- do.call(sprintf,c(fmt = base_string, as.list(v1)))
-#  barcode = c(barcode,temp)
-#}
-
-#mat2 <- unlist(intersect(emInfo$bcr_patient_barcode,barcode))
-#purity_from_file <- tcga_purity[match(mat2,barcode),purity]
-#purity_abs <- emInfo[match(mat2,emInfo$bcr_patient_barcode), 'abs_purity']
-#filena <- which(is.na(purity_from_file))
-#absna <- which(is.na(purity_abs))
-#nona <- c(filena,absna)
-#purity_from_file <- purity_from_file[-nona]
-#purity_abs <- purity_abs[-nona]
-# correlation between purity_abs and purity_file = 0.92
-
-#pdf('../../figures/AbsVSFile.pdf')
-#tempdata = cbind(purity_from_file, purity_abs)
-#colnames(tempdata) <- c("Pan_cancerPurity","AbsolutePurity")
-#newplot <- ggplot(data = as.data.frame(tempdata), aes(x=Pan_cancerPurity,y=AbsolutePurity))+ xlim(0,1) + ylim(0,1) +
-#  geom_point() + geom_abline(intercept = 0,slope = 1)
-#print(newplot)
-#dev.off()
-
-#setwd("~/Hutch-Research/Data/Real")
-#pc = readRDS('pan_cancer_somatic_mutation_summary.rds')
-#purity = fread('./methylation_data/patient_coad_EMC_info_w_plate_nMut.txt')
-
-#mat3 <- unlist(intersect(pc$barcode,barcode))
-#purity_est_all <- pc[match(mat3, pc$barcode),'AscatPurity']
-#purity_file_all <- as.matrix(tcga_purity[match(mat3,barcode),'purity'])
-#estna <- which(is.na(purity_est_all))
-#fileallna <- which(is.na(purity_file_all))
-#allna <- c(estna,fileallna)
-#purity_est_all <- purity_est_all[-allna]
-#purity_file_all <- purity_file_all[-allna]
-# correlation is 0.86
-
-#pdf('../../figures/EstVSFile.pdf')
-#tempdata = cbind(purity_file_all, purity_est_all)
-#colnames(tempdata) <- c("Pan_cancerPurity","AscatPurity")
-#newplot <- ggplot(data = as.data.frame(tempdata), aes(x=Pan_cancerPurity,y=AscatPurity))+ xlim(0,1) + ylim(0,1) +
- # geom_point() + geom_abline(intercept = 0,slope = 1)
-#print(newplot)
-#dev.off()
-
-
-
-
+deconv_expr[1:2,]
 
 
 
