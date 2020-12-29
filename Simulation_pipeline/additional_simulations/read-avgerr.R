010ec82d5daad01f2e63a8615094d23469ad1832
Wei Sun <weisun2013@gmail.com>
2020-12-29 13:06:13 -0800

    update on simulation data analysis

--- Simulation_pipeline/additional_simulations/read-avgerr.R
+++ Simulation_pipeline/additional_simulations/read-avgerr.R
@@ -1,9 +1,24 @@
 # sigma2 is the variance of normal CpGs
 # lambda = sigma_aberrant^2 / sigma_consistent^2
 
+# install.packages("stargazer")
+library(stargazer)
+
 load("avgerr.RData")
 noiselevel = dimnames(avgerr)[1]
+
+dim(avgerr)
+avgerr[1,,,1]
+
+stargazer(t(avgerr[1,-2,,1]), summary=FALSE)
+
 for(i in 1:6){
   cat("MSE when Noise level = ", noiselevel[[1]][i],"------------\n")
-  print(t(avgerr[i,,,1]))
+  stargazer(t(avgerr[i,-2,,1]), summary=FALSE)
 }
+
+
+sessionInfo()
+gc()
+
+quit(save = 'no')
